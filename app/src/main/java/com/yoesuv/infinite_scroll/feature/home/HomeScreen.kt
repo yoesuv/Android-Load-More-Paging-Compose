8a90fe844fa95a457af36722cad955e80e09b287
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-07-15 05:05:38 +0700

    Adds item post composable
    Adds a new composable function for displaying a post item with title and description.
    Also, extracts strings to resource file.

--- app/src/main/java/com/yoesuv/infinite_scroll/feature/home/HomeScreen.kt
+++ app/src/main/java/com/yoesuv/infinite_scroll/feature/home/HomeScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.yoesuv.infinite_scroll.compose.R
 
 @Composable
 fun HomeScreen(
@@ -29,7 +31,7 @@ fun HomeScreen(
         Button(
             onClick = onPaginationListClick
         ) {
-            Text(text = "Pagination List")
+            Text(text = stringResource(R.string.pagination_list))
         }
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -37,7 +39,7 @@ fun HomeScreen(
         Button(
             onClick = onPaginationGridClick
         ) {
-            Text(text = "Pagination Grid")
+            Text(text = stringResource(R.string.pagination_grid))
         }
     }
 }
---
+++ app/src/main/java/com/yoesuv/infinite_scroll/feature/paging_list/ItemPost.kt
@@ -0,0 +1,40 @@
+package com.yoesuv.infinite_scroll.feature.paging_list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ItemPost(text1: String, text2: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(
+            text = text1,
+            style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Medium, color = Color.Black)
+        )
+        Text(
+            text = text2,
+            style = TextStyle(fontSize = 12.sp, color = Color.Black.copy(alpha = 0.8F))
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemPostPreview() {
+    ItemPost(text1 = "Hello", text2 = "Lorem ipsum dolor sit amit")
+}
