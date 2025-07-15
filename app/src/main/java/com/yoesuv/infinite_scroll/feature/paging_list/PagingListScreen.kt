33ee07afa173c3fb72d6f5c9de2c44340d44ad5e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-07-15 15:45:12 +0700

    Improves PagingListScreen UI and error handling
    Enhances the PagingListScreen by adding a top bar for better user experience.
    
    Wraps the LazyColumn with a Scaffold and adds a top bar with title.
    Also, adds error handling to display an error message when loading fails.

--- app/src/main/java/com/yoesuv/infinite_scroll/feature/paging_list/PagingListScreen.kt
+++ app/src/main/java/com/yoesuv/infinite_scroll/feature/paging_list/PagingListScreen.kt
@@ -1,33 +1,37 @@
 package com.yoesuv.infinite_scroll.feature.paging_list
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.yoesuv.infinite_scroll.compose.R
+import com.yoesuv.infinite_scroll.feature.widgets.AppTopBar
 
 @Composable
 fun PagingListScreen(viewModel: PagingListViewModel) {
     val pagingItems = viewModel.posts.collectAsLazyPagingItems()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
+    Scaffold(
+        topBar = { AppTopBar(title = stringResource(R.string.pagination_list)) }
+    ) { innerPadding ->
         LazyColumn(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .padding(paddingValues = innerPadding)
+                .fillMaxSize()
         ) {
             items(pagingItems.itemCount) { index ->
                 val post = pagingItems[index]
@@ -40,14 +44,21 @@ fun PagingListScreen(viewModel: PagingListViewModel) {
                 is LoadState.Loading -> {
                     item {
                         Box(
-                            modifier = Modifier.fillMaxSize(),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 16.dp),
                             contentAlignment = Alignment.Center
                         ) { CircularProgressIndicator(modifier = Modifier.size(32.dp)) }
                     }
                 }
 
                 is LoadState.Error -> {
-                    item { Text("Error loading posts", color = MaterialTheme.colorScheme.error) }
+                    item {
+                        Text(
+                            "Error loading posts",
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    }
                 }
 
                 else -> {}
@@ -79,5 +90,4 @@ fun PagingListScreen(viewModel: PagingListViewModel) {
             }
         }
     }
-
 }
