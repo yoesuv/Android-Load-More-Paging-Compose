ad246c9e82bdd28b44692a4d55f448419f275940
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-07-16 11:22:41 +0700

    Updates theme to use Teal500 as primary color
    Changes the primary color of the application theme to Teal500 for both light and dark color schemes.
    
    Disables dynamic color theming.

--- app/src/main/java/com/yoesuv/infinite_scroll/MainActivity.kt
+++ app/src/main/java/com/yoesuv/infinite_scroll/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            InfiniteScrollTheme {
+            InfiniteScrollTheme (dynamicColor = false) {
                 AppNavigation()
             }
         }
--- app/src/main/java/com/yoesuv/infinite_scroll/core/theme/Theme.kt
+++ app/src/main/java/com/yoesuv/infinite_scroll/core/theme/Theme.kt
@@ -11,15 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Teal500,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Teal500,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
